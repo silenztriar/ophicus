73344a0c2cf903b48b9f836afcdccce91a62b789
silenztriar <49186115+silenztriar@users.noreply.github.com>
2019-12-15 19:49:36 +0100

    Add files via upload

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://{ophicus}.herokuapp.com/execute.php';
+$BOT_TOKEN = '{987157543:AAHW8xthl9PIiv147LWGW3ABh5hVYdkp4EY}';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
